a83a193af5e30a66fa53bcb2aeade98a2cd946a5
Christopher Barnes <senrabc@gmail.com>
2016-04-22 09:13:45 -0400

    getting apa citation format from doi.org

--- doi.org.sh
+++ doi.org.sh
@@ -3,6 +3,14 @@
 url="dx.doi.org/"
 for i in $(cat vivo-doi.txt); do
     sleep .5
-    content="$(curl -LH "Accept: text/bibliography; style=apa" "$url/$i")"
-    echo "$content" >> apa-output.txt
+    content="$(curl -LH "Accept: text/bibliography; style=apa" "$url/$i" || true)"
+    echo "*********************************************************************"
+    echo "$content"
+    echo "*********************************************************************"
+     # add logic here to catch exceptions to return from curl
+    if  grep '<html>' $content 1> /dev/null; then
+	echo "this doi has an error: $i" >> exception_log.txt 
+    else
+    	echo "$content" >> apa-output.txt
+    fi
 done
